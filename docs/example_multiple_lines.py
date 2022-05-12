7979019298c0e94032e42cab54b9aa156439e51b
Peter Lombaers <peter@idfuse.nl>
2022-05-12 16:44:20 +0200

    Add multiple lines example
    Add an example of how to have multiple lines
    in one plot. Also adds docstring to recall,
    and fixes some typos.

---
+++ docs/example_multiple_lines.py
@@ -0,0 +1,25 @@
+# An example of plotting the recall curves for two ASREVIEW files in one plot,
+# with a legend.
+
+import matplotlib.pyplot as plt
+
+from asreview import open_state
+from asreviewcontrib.insights.plot import plot_recall
+
+
+fig, ax = plt.subplots()
+
+with open_state("tests/asreview_files/sim_van_de_schoot_2017_1.asreview") as s1:
+    plot_recall(ax, s1)
+
+with open_state("tests/asreview_files/"
+                "sim_van_de_schoot_2017_logistic.asreview") as s2:
+    plot_recall(ax, s2)
+
+# Set the labels for the legend. Both plots add the recall line and the random
+# line. Hence the recall lines are the 0th and 2nd line.
+ax.lines[0].set_label("Naive Bayes")
+ax.lines[2].set_label("Logistic")
+ax.legend()
+
+fig.savefig("docs/example_multiple_lines.png")
