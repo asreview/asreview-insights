5e4a10f62165210d17a826218869da3a75f7d149
PeterLombaers <71253799+PeterLombaers@users.noreply.github.com>
2022-12-08 13:49:52 +0100

    Fix priors bug in pad_simulation_labels
    The pad_simulation_labels functions added too many zeroes if the priors are not used. This fixes that bug.

--- asreviewcontrib/insights/utils.py
+++ asreviewcontrib/insights/utils.py
@@ -8,10 +8,15 @@ def pad_simulation_labels(state_obj, priors=False):
 
     # get the labels
     labels = state_obj.get_labels(priors=priors).to_list()
+    
+    if not priors:
+        n_used_records = n_records - state_obj.n_priors
+    else:
+        n_used_records = n_records
 
     # if less labels than records, check if all labels available
-    if len(labels) < n_records:
+    if len(labels) < n_used_records:
 
-        labels = labels + np.zeros(n_records - len(labels)).tolist()
+        labels = labels + np.zeros(n_used_records - len(labels)).tolist()
 
     return labels
