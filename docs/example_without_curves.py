4c0620c6a1d7027fbd157529f01c51b531dca843
Jelle Teijema <j.j.teijema@uu.nl>
2024-09-26 18:58:00 +0200

    Perfect line in recall plot (#65)
    Co-authored-by: Jonathan de Bruin <jonathandebruinos@gmail.com>

---
+++ docs/example_without_curves.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+from asreview import open_state
+
+from asreviewcontrib.insights.plot import plot_recall
+
+with open_state("tests/asreview_files/sim_van_de_schoot_2017_logistic.asreview") as s:
+    fig, ax = plt.subplots()
+    plot_recall(ax, s, show_random=False, show_perfect=False)
+
+    fig.savefig("docs/example_without_curves.png")
