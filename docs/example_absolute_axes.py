ccb8783e2a99a90bfa5b8e1efc8098ad785ea485
Jelle Teijema <j.j.teijema@uu.nl>
2024-09-26 18:21:35 +0200

    Update the insights workflows and examples. (#67)

--- docs/example_absolute_axes.py
+++ docs/example_absolute_axes.py
@@ -3,7 +3,7 @@ from asreview import open_state
 
 from asreviewcontrib.insights.plot import plot_recall
 
-with open_state("tests/asreview_files/sim_van_de_schoot_2017_1.asreview") as s:
+with open_state("tests/asreview_files/sim_van_de_schoot_2017_logistic.asreview") as s:
     fig, ax = plt.subplots()
     plot_recall(ax, s, x_absolute=True, y_absolute=True)
 
