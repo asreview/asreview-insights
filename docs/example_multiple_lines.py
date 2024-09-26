bc7d583ab4db4c86fab94baef7d5e924a919aba8
jteijema <j.j.teijema@uu.nl>
2024-09-26 14:47:57 +0200

    revert change to documents

--- docs/example_multiple_lines.py
+++ docs/example_multiple_lines.py
@@ -8,9 +8,7 @@ from asreviewcontrib.insights.plot import plot_recall
 
 fig, ax = plt.subplots()
 
-with open_state(
-    "tests/asreview_files/sim_van_de_schoot_2017_stop_if_min.asreview"
-) as s1:
+with open_state("tests/asreview_files/sim_van_de_schoot_2017_1.asreview") as s1:
     plot_recall(ax, s1)
 
 with open_state(
@@ -18,10 +16,10 @@ with open_state(
 ) as s2:
     plot_recall(ax, s2)
 
-# Set the labels for the legend. Both plots add the recall line, the random
-# curve, and the perfect curve. Hence the recall lines are the 0th and 3nd line.
+# Set the labels for the legend. Both plots add the recall line and the random
+# line. Hence the recall lines are the 0th and 2nd line.
 ax.lines[0].set_label("Naive Bayes")
-ax.lines[3].set_label("Logistic")
+ax.lines[2].set_label("Logistic")
 ax.legend()
 
 fig.savefig("docs/example_multiple_lines.png")
