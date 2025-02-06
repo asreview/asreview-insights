9115acabf0aa7ec3cbfc56bcc3645e2b7e8a3ffc
Jelle Teijema <j.j.teijema@uu.nl>
2025-02-06 16:31:01 +0100

    Update entrypoint.py with float defaults

--- asreviewcontrib/insights/entrypoint.py
+++ asreviewcontrib/insights/entrypoint.py
@@ -156,7 +156,7 @@ class MetricsEntryPoint(BaseEntryPoint):
             metavar="cm",
             type=float,
             nargs="+",
-            default=[0.1, 0.25, 0.5, 0.75, 0.8, 0.85, 0.9, 0.95, 1],
+            default=[0.1, 0.25, 0.5, 0.75, 0.8, 0.85, 0.9, 0.95, 1.0],
             help="A (list of) values to compute the cm at.",
         )
         parser.add_argument(
